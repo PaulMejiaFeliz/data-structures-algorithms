87251499f5ed162322683a1f648b7d30a40ec7ba
Paul Mejia <paulmejiafeliz@gmail.com>
2022-08-14 23:02:57 -0400

    feat: Add Big O example

---
+++ src/big-o/excercises.js
@@ -0,0 +1,40 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  // O(n)
+  for (let i = 0; i < input.length; i++) {
+    anotherFunction();
+    let stranger = true;
+    a++;
+  }
+
+  return a;
+}
+
+// O(n)
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+
+  // O(n)
+  for (let i = 0; i < input; i++) {
+    let x = i + 1;
+    let y = i + 2;
+    let z = i + 3;
+  }
+
+  // O(n)
+  for (let j = 0; j < input; j++) {
+    let p = j * 2;
+    let q = j * 2;
+  }
+
+  let whoAmI = "I don't know"; // O(1)
+}
+
+// O(n)
