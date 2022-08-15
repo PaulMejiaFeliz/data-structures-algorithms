20b08a227e51d1f34b9756e6ace6b61a914790e0
Paul Mejia <paulmejiafeliz@gmail.com>
2022-08-14 22:16:35 -0400

    fix: Rename linear time example

--- src/big-o/linear-time.ts
+++ src/big-o/linear-time.ts
@@ -16,20 +16,15 @@ const everyone = [
 const large = new Array(1000).fill('nemo');
 
 function findNemo(array: string[]) {
-  const t0 = performance.now();
-
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found NEMO!');
     }
   }
-
-  const t1 = performance.now();
-  console.log(`Call to findNemo took ${t1 - t0} milliseconds`);
 }
 
 console.log('Finding Nemo');
 
-findNemo(nemo);
-findNemo(everyone);
-findNemo(large);
+// findNemo(nemo);
+// findNemo(everyone);
+findNemo(large); // O(n) --> Linear Time
