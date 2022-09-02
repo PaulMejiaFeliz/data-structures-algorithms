f8a6bd83fe8267d75a5f7bd81d493ab63af5dce9
Paul Mejia <paulmejiafeliz@gmail.com>
2022-09-01 21:01:18 -0400

    feat: Implement reverse linked list

--- src/data-strucures/linked-lists/singly-linked-list.ts
+++ src/data-strucures/linked-lists/singly-linked-list.ts
@@ -110,6 +110,26 @@ class LinkedList<T> {
     return currentNode;
   }
 
+  reverse(): LinkedList<T> {
+    let nextNode: LinkedListNode<T> | undefined = this.head;
+    let node = nextNode.next;
+
+    while (node) {
+      const prevNode = node.next;
+      node.next = nextNode;
+      nextNode = node;
+      node = prevNode;
+    }
+
+    const tail = this.head;
+    tail.next = undefined;
+
+    this.head = this.tail;
+    this.tail = tail;
+
+    return this;
+  }
+
   toArray(): T[] {
     const values: T[] = [];
 
@@ -123,19 +143,55 @@ class LinkedList<T> {
   }
 }
 
+console.log('Creates List with 10');
 const myLinkedList = new LinkedList(10);
+
+console.log(myLinkedList.toArray());
+console.log('Appends 5 and 16');
 
 myLinkedList.append(5);
 myLinkedList.append(16);
+
+console.log(myLinkedList.toArray());
+console.log('Prepends 1');
+
 myLinkedList.prepend(1);
+
+console.log(myLinkedList.toArray());
+console.log('Inserts 99 in 2');
+
 myLinkedList.insert(2, 99);
+
+console.log(myLinkedList.toArray());
+console.log('Inserts 30 in 0');
+
 myLinkedList.insert(0, 30);
+
+console.log(myLinkedList.toArray());
+console.log('Inserts 50 at the end');
+
 myLinkedList.insert(myLinkedList.length, 50);
 
 console.log(myLinkedList.toArray());
+console.log('Reverses list');
+
+myLinkedList.reverse();
+
+console.log(myLinkedList.toArray());
+console.log('Removes 4');
 
 myLinkedList.remove(4);
+
+console.log(myLinkedList.toArray());
+console.log('Removes 0');
+
 myLinkedList.remove(0);
+
+console.log(myLinkedList.toArray());
+console.log('Removes last');
+
 myLinkedList.remove(myLinkedList.length - 1);
 
 console.log(myLinkedList.toArray());
+
+export {};
