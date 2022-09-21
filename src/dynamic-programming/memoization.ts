479bf330de141fb24bdb8f829a2b86da0126df09
Paul Mejia <paulmejiafeliz@gmail.com>
2022-09-20 22:32:17 -0400

    refactor: Update memoization example

--- src/dynamic-programming/memoization.ts
+++ src/dynamic-programming/memoization.ts
@@ -1,13 +1,16 @@
-const cache: { [key: number]: number } = {};
+function memoizedAdd80to() {
+  const cache: { [key: number]: number } = {};
+  return (n: number) => {
+    if (n in cache === false) {
+      console.log('Long Operation');
+      cache[n] = n + 80;
+    }
 
-function add80to(n: number) {
-  if (n in cache === false) {
-    console.log('Long Operation');
-    cache[n] = n + 80;
-  }
-
-  return cache[n];
+    return cache[n];
+  };
 }
+
+const add80to = memoizedAdd80to();
 
 console.log(add80to(5));
 console.log(add80to(5));
